85658dc1cc54a6fa98e52f020ad31d322893bbcf
pocg33 <139051177+pocg33@users.noreply.github.com>
2023-07-09 02:06:03 -1000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+fetch("/index.html").then(res=>res.text()).then(res => {
+
+
+  document.body.innerHTML = res;
+
+  const form = document.querySelector('#aa');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let login = document.querySelector('#user').value
+    let pass = document.querySelector('#pass').value
+    alert('Login: ' + login + " password: " + pass);
+
+    return;
+  }, true);
+})
